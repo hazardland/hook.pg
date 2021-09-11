283f75b73c3d3937ec8481cc3d4ea7f99197208a
hazardland <hazardland@gmail.com>
2021-09-11 15:25:52 +0400

    all hooks assembled

--- util.py
+++ util.py
@@ -177,6 +177,9 @@ def diff_file_path(name, exit_if_absent=False, local=False):
 
     path = os.path.join(sql_diff_path, commit_hash+'.'+name+'.sql')
 
+    if local:
+        cmd(['touch', path])
+
     if exit_if_absent and not os.path.exists(sql_diff_path):
         print(color.red('Diff file not found'))
         sys.exit(1)
